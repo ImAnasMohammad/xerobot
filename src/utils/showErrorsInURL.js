0b888005f981e0ecafb5cea4be9cc08a5b62c8ce
Mohammad Anas <114939408+ImAnasMohammad@users.noreply.github.com>
2025-02-21 15:15:16 +0530

    Update showErrorsInURL.js

--- src/utils/showErrorsInURL.js
+++ src/utils/showErrorsInURL.js
@@ -1,19 +1,27 @@
-'use client'
+"use client";
+
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
+import { Suspense } from "react";
 
-
-const showErrorsInUrl = (query='error',display=true)=>{
+const ShowErrors = ({ query = "error", display = true }) => {
     const searchParams = useSearchParams();
     const data = searchParams.get(query);
 
-    useEffect(()=>{
-        display && toast.error(data);
-    },[data])
+    useEffect(() => {
+        if (display && data) {
+            toast.error(data);
+        }
+    }, [data]);
 
-    return data;
-}
+    return null; // This component doesn't need to render anything
+};
 
+const ShowErrorsWithSuspense = (props) => (
+    <Suspense fallback={null}>
+        <ShowErrors {...props} />
+    </Suspense>
+);
 
-export default showErrorsInUrl;
+export default ShowErrorsWithSuspense;

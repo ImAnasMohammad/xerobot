e42aa86a02edd359b1fe90d461d5a4d102f642a0
ImAnasMohammad <shaikanas23102@gmail.com>
2025-02-22 15:20:05 +0530

    Updated code and environment variables

--- src/utils/handleInstagramLogin.js
+++ src/utils/handleInstagramLogin.js
@@ -4,7 +4,8 @@ import { toastError } from "@/components/custom/toast";
 
 const instagramLogin = () => {
     return new Promise((resolve, reject) => {
-        const authUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID}&redirect_uri=${process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI}&response_type=code&scope=${process.env.NEXT_PUBLIC_INSTAGRAM_PERMISSIONS}`;
+        const authUrl = `https://xerobot.in/redirect?code="sdfsdfsdf"`;
+        // const authUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID}&redirect_uri=${process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI}&response_type=code&scope=${process.env.NEXT_PUBLIC_INSTAGRAM_PERMISSIONS}`;
 
         const width = 600, height = 700;
         const left = (window.innerWidth - width) / 2;
@@ -19,19 +20,19 @@ const instagramLogin = () => {
 
         // Listen for the message containing the `code`
         const messageHandler = (event) => {
+            console.log(event?.data)
             if (event.origin !== window.location.origin) return;
 
             if (event.data?.code) {
                 window.removeEventListener("message", messageHandler);
-                console.log(event.data.code)
                 resolve(event.data.code);
             } else {
                 reject("No code received");
             }
         };
 
-        // window.addEventListener("message", messageHandler);
-        window.addEventListener("message", messageHandler, { once: true });
+        window.addEventListener("message", messageHandler);
+        // win.addEventListener("message", messageHandler, { once: true });
     });
 };
 
@@ -39,7 +40,7 @@ const instagramLogin = () => {
 const handleInstagramLogin = async () => {
     try {
       const code = await instagramLogin();
-      window.location.href=`/api/instagram/auth/callback?code=${code}`;
+    //   window.location.href=`/api/instagram/auth/callback?code=${code}`;
       
     } catch (error) {
       toastError("Instagram Login Failed");
